5b691e61dd072612f8a83df57409bc56963981f2
Marina Galvagni <marina.galvagni@ansys.com>
2022-06-07 16:53:10 -0400

    Change the directory structure from ansys/ensight/core to ansys/pyensight: add files

---
+++ src/ansys/pyensight/__init__.py
@@ -0,0 +1,26 @@
+"""This init file allows python to treat directories containing it as modules.
+
+Import any methods you want exposed at your library level here.
+
+For example, if you want to avoid this behavior:
+
+.. code::
+
+   >>> from ansys.pyensight.module import add
+
+Then add the import within this module to enable:
+
+.. code::
+
+   >>> import ansys.pyensight as pyensight
+   >>> pyensight.add(1, 2)
+
+"""
+
+version_info = 0, 1, "dev0"
+
+# Nice string for the version
+__version__ = ".".join(map(str, version_info))
+
+from ansys.pyensight.module import add
+from ansys.pyensight..other_module import Complex
