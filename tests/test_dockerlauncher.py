f9dc3db8cf176cdd434b8a0e9887b96a06e4a3d9
Mario Ostieri <mario.ostieri@ansys.com>
2023-02-16 15:37:59 +0000

    basic unit tests to check the API without launching EnSight. 84% coverage

---
+++ tests/test_dockerlauncher.py
@@ -0,0 +1,81 @@
+from unittest import mock
+
+import docker
+import pytest
+
+import ansys.pyensight
+from ansys.pyensight import DockerLauncher
+
+
+def test_start(mocker, capsys):
+    docker_client = mock.MagicMock("Docker")
+    run = mock.MagicMock("MockedRun")
+    run.exec_run = mock.MagicMock("MockedExec")
+    path = "/ansys_inc/v345/CEI/bin/ensight"
+    cei_home = path.encode("utf-8")
+    values = [
+        [0, cei_home],  # first run, to find the ensight version
+        [0, "ensight_run"],  # second run
+        [0, "websocket_run"],
+    ]
+    returned = mocker.patch.object(run, "exec_run", side_effect=values.copy())
+    docker_client.containers = mock.MagicMock("MockedContainers")
+    docker_client.containers.run = mock.MagicMock("MockedContainer")
+    mocker.patch.object(docker_client.containers, "run", return_value=run)
+    dock = mocker.patch.object(docker, "from_env", return_value=docker_client)
+    launcher = DockerLauncher(data_directory=".")
+    launcher = DockerLauncher(
+        data_directory=".", use_dev=True, docker_image_name="super_ensight"
+    )
+    mocked_session = mock.MagicMock("MockedSession")
+    mocker.patch.object(ansys.pyensight, "Session", return_value=mocked_session)
+    assert launcher.start() == mocked_session
+    out, err = capsys.readouterr()
+    assert "Ansys Version= 345" in out
+    assert "Run:  ['bash', '--login', '-c', ' ensight -batch -v 3" in out
+    assert (
+        "Run:  ['bash', '--login', '-c', 'cpython /ansys_inc/v345/CEI/nexus345/nexus_launcher/websocketserver.py --http_directory /home/ensight"
+        in out
+    )
+    assert launcher.ansys_version() == "345"
+    values[0][0] = 1
+    returned.side_effect = values.copy()
+    with pytest.raises(RuntimeError) as exec_info:
+        launcher.start()
+    assert "find /ansys_inc/vNNN/CEI/bin/ensight in the Docker container." in str(
+        exec_info
+    )
+    values[0][0] = 0
+    values[0][1] = "/path/to/darkness".encode("utf-8")
+    returned.side_effect = values.copy()
+    with pytest.raises(RuntimeError) as exec_info:
+        launcher.start()
+    assert "find version from cei_home in the Docker container." in str(exec_info)
+    dock = mocker.patch.object(docker, "from_env", return_value=docker_client)
+    dock.side_effect = ModuleNotFoundError
+    with pytest.raises(RuntimeError) as exec_info:
+        launcher = DockerLauncher(data_directory=".")
+    assert "The pyansys-docker module must be installed for DockerLauncher" in str(
+        exec_info
+    )
+    dock.side_effect = KeyError
+    with pytest.raises(RuntimeError) as exec_info:
+        launcher = DockerLauncher(data_directory=".")
+    assert "Cannot initialize Docker" in str(exec_info)
+
+
+def test_pull(mocker):
+    def simulate_network_issue(image):
+        raise ConnectionAbortedError
+
+    docker_client = mock.MagicMock("Docker")
+    docker_client.images = mock.MagicMock("ImagesInterface")
+    docker_client.images.pull = mock.MagicMock("Pull")
+    docker_client.images.pull = lambda unused: True
+    dock = mocker.patch.object(docker, "from_env", return_value=docker_client)
+    launcher = DockerLauncher(data_directory=".")
+    launcher.pull()
+    docker_client.images.pull = simulate_network_issue
+    with pytest.raises(RuntimeError) as exec_info:
+        launcher.pull()
+    assert "pull Docker image: ghcr.io/ansys/ensight" in str(exec_info)
---
+++ tests/test_locallauncher.py
@@ -0,0 +1,69 @@
+import glob
+import os
+import platform
+import shutil
+import subprocess
+import time
+from unittest import mock
+
+import pytest
+
+import ansys.pyensight
+from ansys.pyensight import LocalLauncher
+
+
+def test_start(mocker):
+    mocker.patch.object(
+        LocalLauncher, "get_cei_install_directory", return_value="/path/to/awp/CEI"
+    )
+    launcher = LocalLauncher("/path/to/awp/")
+    popen = mock.MagicMock("MockedPopen")
+    popen.pid = 3456
+    mocker.patch.object(subprocess, "Popen", return_value=popen)
+    glob_mock = mock.MagicMock("superGlob")
+    glob_mock.side_effect = ["/path/to/awp/CEI/nexus345/websocketserver.py"]
+    mocker.patch.object(glob, "glob", glob_mock)
+    mocker.patch.object(ansys.pyensight, "Session")
+    # Mocking Popen breaks platform.system, so the function is mocked
+    system = mocker.patch.object(platform, "system", return_value="Windows")
+    launcher.start()
+    system = mocker.patch.object(platform, "system", return_value="Linux")
+    glob_mock.side_effect = ["/path/to/awp/CEI/nexus345/websocketserver.py"]
+    launcher = LocalLauncher("/path/to/awp/", batch=False)
+    launcher.start()
+
+
+def test_stop(mocker, tmpdir):
+    mocker.patch.object(
+        LocalLauncher, "get_cei_install_directory", return_value="/path/to/awp/CEI"
+    )
+    session_dir = tmpdir.mkdir("session_dir")
+    launcher = LocalLauncher("/path/to/awp/")
+    launcher.session_directory = session_dir
+    launcher._ports = [1111, 2222]
+    launcher.stop()
+    assert not os.path.exists(session_dir)
+    assert launcher._ports is None
+
+
+def test_get_cei_install_directory(mocker):
+    exists = mocker.patch.object(os.path, "exists", return_value=True)
+    path = "/path/to/darkness"
+    method = LocalLauncher.get_cei_install_directory
+    assert method(path) == os.path.join(path, "CEI")
+    second_path = "/stairway/to/heaven"
+    os.environ["PYENSIGHT_ANSYS_INSTALLATION"] = second_path
+    assert method(path) == os.path.join(path, "CEI")
+    assert method(None) == os.path.join(second_path)
+    del os.environ["PYENSIGHT_ANSYS_INSTALLATION"]
+    version = ansys.pyensight.__ansys_version__
+    # In case tests are launched locally and the environment variable is
+    # set, the launcher would manage to find an install. This trick
+    # cleans it, to check all the paths
+    if f"AWP_ROOT{version}" in os.environ:
+        del os.environ[f"AWP_ROOT{version}"]
+    os.environ[f"AWP_ROOT{version}"] = second_path
+    assert method(None) == os.path.join(second_path, "CEI")
+    exists.return_value = False
+    with pytest.raises(RuntimeError):
+        method(None)
