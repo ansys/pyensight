27a1230c72935ee7d0d6827530c4652b75fd8956
U-ANSYS\mostieri <mostieri@AAPvXK2h7onQGp7.win.ansys.com>
2023-02-28 11:27:54 +0000

    fix docker container cleaning

--- .ci/pull_ensight_image.sh
+++ .ci/pull_ensight_image.sh
@@ -4,10 +4,10 @@ set -ex
 # Image name
 _IMAGE_NAME="ghcr.io/ansys/ensight_dev"
 
+# Remove all dangling images
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker image prune -f
+
 # Pull nexus image based on tag
 docker pull $_IMAGE_NAME
-
-# Remove all dangling images
-docker container stop $(docker container ls -aq)
-docker container prune -f
-docker image prune -f
