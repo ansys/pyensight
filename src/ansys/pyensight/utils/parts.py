3025b48a62e1e5a7e7143c29a404e27b10f95669
U-ANSYS\mostieri <mostieri@AAPvXK2h7onQGp7.win.ansys.com>
2023-06-09 13:01:03 +0100

    first draft

--- src/ansys/pyensight/utils/parts.py
+++ src/ansys/pyensight/utils/parts.py
@@ -13,18 +13,23 @@ Example to select all the 3D parts:
 """
 
 import math
-from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
+from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, Union
+
+from ansys.pyensight.listobj import ensobjlist
 
 if TYPE_CHECKING:
-    from ansys.pyensight.session import Session
+    try:
+        import ensight
+    except ImportError:
+        from ansys.pyensight import ensight_api
 
     
 class Parts:
     """A class to handle the parts in the current EnSight session."""
 
-    def __init__(self, ensight: "Session.ensight"):
+    def __init__(self, ensight: Union["ensight_api.ensight", "ensight"]):
         self.ensight = ensight
 
     def select_parts_by_dimension(self, dimension: int) -> None:
         parts = self.ensight.objs.core.PARTS
-        parts[0].
+        return ensobjlist([p for p in parts if getattr(p, f"HAS{dimension}DELEMENTS") == True])
