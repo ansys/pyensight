e66c137a9db2574b308a1b7d67ad2825fdb98476
afernand <alex.fernandezluces@ansys.com>
2023-06-16 10:10:11 +0200

    Docs to numpydoc

--- src/ansys/pyensight/core/launch_ensight.py
+++ src/ansys/pyensight/core/launch_ensight.py
@@ -47,17 +47,21 @@ if pim_is_available:
         Start via PyPIM the EnSight Docker container with EnShell as the ENTRYPOINT.
         Create and bind a Session instance to the created gRPC session.  Return that session.
 
-        Args:
-          product_version : str, optional
+        Parameters
+        ----------
+        product_version : str, optional
             Version of the product. For example, "232". The default is "None", in which case
-          use_egl:
+        use_egl : bool, optional
             If True, EGL hardware accelerated graphics will be used. The platform
             must be able to support it.
-          use_sos:
+        use_sos : int, optional
             If None, don't use SOS. Otherwise, it's the number of EnSight Servers to use (int).
 
-        Returns:
-          pyensight Session object instance
+        Returns
+        -------
+
+            pyensight Session object instance
+
         """
 
         pim = pypim.connect()
@@ -102,49 +106,54 @@ def launch_ensight(
     """Start an EnSight session via EnShell using the Docker EnSight Image.
     Return that session.
 
-    Args:
-      product_version : str, optional
+    Parameters
+    ----------
+    product_version : str, optional
         Select an installed version of ANSYS. The string must be in a format like
         "232" (for 2023 R2). The default is "None", in which case the newest installed
         version is used.
-      use_pim : bool, optional
+    use_pim : bool, optional
         If True, then PyPIM is used to launch EnSight.
-      use_docker : bool, optional
+    use_docker : bool, optional
         If True, use DockerLaucherEnShell. If use_pim is True, this option is ignored.
-      data_directory:
+    data_directory: str, optional
         Host directory to make into the Docker container at /data
         Only used if use_docker is True.
-      docker_image_name:
+    docker_image_name: str, optional
         Optional Docker Image name to use
-      use_dev:
+    use_dev: bool, optional
         Option to use the latest ensight_dev Docker Image; overridden by docker_image_name if specified.
-      ansys_installation:
-        Location of the ANSYS installation, including the version
+    ansys_installation: str, optional
+        Location of the ANSYS installation, including the version.
         directory Default: None (causes common locations to be scanned).
         If use_pim is True, this option is ignored.  If use_docker is True, this option is ignored.
-      application:
+    application: str, optional
         The application to be launched. By default, "ensight", but
         "envision" is also available.
-      batch:
+    batch: bool, optional
         By default, the EnSight/EnVision instance will run in batch mode.
         If batch is set to False, the full GUI will be presented.
         Only used if use_pim and use_docker are False.
-      use_egl:
+    use_egl: bool, optional
         If True, EGL hardware accelerated graphics will be used. The platform
         must be able to support it.
-      use_sos:
+    use_sos: int, optional
         If None, don't use SOS. Otherwise, it's the number of EnSight Servers to use (int).
-      timeout:
+    timeout: float, optional
         In some cases where the EnSight session can take a significant amount of
         time to start up, this is the number of seconds to wait before failing
         the connection.  The default is 120.0.
 
-    Returns:
-      pyensight Session object instance
+    Returns
+    -------
+    type
+        pyensight Session object instance
 
-    Raises:
-      RuntimeError:
-          variety of error conditions.
+    Raises
+    ------
+    RuntimeError
+        variety of error conditions
+
     """
 
     # print(f"pim_is_available: {pim_is_available}  use_pim: {use_pim}\n")
