db3a01c1dbaeeceec0b0ddd072b001b8f334d9e3
afernand <alex.fernandezluces@ansys.com>
2023-06-23 12:58:01 +0200

    Fix version file

--- src/ansys/pyensight/core/_version.py
+++ src/ansys/pyensight/core/_version.py
@@ -2,7 +2,7 @@ import os
 
 import toml
 
-root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 toml_file = os.path.join(root, "pyproject.toml")
 VERSION = toml.load(toml_file)["project"]["version"]
 DEFAULT_ANSYS_VERSION = "241"
