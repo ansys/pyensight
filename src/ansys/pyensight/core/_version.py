9de7ae500969df3b9e8ad58482198725befeee90
afernand <alex.fernandezluces@ansys.com>
2023-06-23 13:12:30 +0200

    Fix version

--- src/ansys/pyensight/core/_version.py
+++ src/ansys/pyensight/core/_version.py
@@ -1,8 +1,9 @@
-import os
+# TODO: undo when uploaded to pypi
+# try:
+#     import importlib.metadata as importlib_metadata  # type: ignore
+# except ModuleNotFoundError:  # pragma: no cover
+#     import importlib_metadata  # type: ignore
+# __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
-import toml
-
-root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-toml_file = os.path.join(root, "pyproject.toml")
-VERSION = toml.load(toml_file)["project"]["version"]
+VERSION = "0.1.dev0"
 DEFAULT_ANSYS_VERSION = "241"
