ad1a79b994093f75083656e5fabfcb7a5968054b
afernand <alex.fernandezluces@ansys.com>
2023-06-23 12:52:20 +0200

    fix version retrieval

--- src/ansys/pyensight/core/_version.py
+++ src/ansys/pyensight/core/_version.py
@@ -1,4 +1,8 @@
-import importlib.metadata as importlib_metadata
+import os
 
+import toml
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+toml_file = os.path.join(root, "pyproject.toml")
+VERSION = toml.load(toml_file)["project"]["version"]
 DEFAULT_ANSYS_VERSION = "241"
-VERSION = importlib_metadata.version(__name__.replace(".", "-"))
