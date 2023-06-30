af9d46e63020a8878219b1eb410462f5124bd8d1
afernand <alex.fernandezluces@ansys.com>
2023-06-30 11:11:15 +0200

    Revert change

--- src/ansys/pyensight/core/dockerlauncher.py
+++ src/ansys/pyensight/core/dockerlauncher.py
@@ -443,6 +443,13 @@ class DockerLauncher(Launcher):
 
         use_egl = self._use_egl()
 
+        # Run EnSight
+        ensight_env = None
+        if use_egl:
+            ensight_env = (
+                "export LD_PRELOAD=/usr/local/lib64/libGL.so.1:/usr/local/lib64/libEGL.so.1 ;"
+            )
+
         ensight_args = "-batch -v 3"
 
         if use_egl:
@@ -457,7 +464,7 @@ class DockerLauncher(Launcher):
         ensight_args += " -vnc " + vnc_url
 
         logging.debug(f"Starting EnSight with args: {ensight_args}\n")
-        ret = self._enshell.start_ensight(ensight_args)
+        ret = self._enshell.start_ensight(ensight_args, ensight_env)
         if ret[0] != 0:
             self.stop()
             raise RuntimeError(f"Error starting EnSight with args: {ensight_args}")
