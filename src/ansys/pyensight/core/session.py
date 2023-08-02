293bd42780a4958baca8a9efbfdcfd5cf93e8515
Mario Ostieri <107915956+mariostieriansys@users.noreply.github.com>
2023-08-02 11:01:45 +0100

    Publish undocumented VNC Angular class (#272)

--- src/ansys/pyensight/core/session.py
+++ src/ansys/pyensight/core/session.py
@@ -34,6 +34,7 @@ from ansys.pyensight.core.renderable import (
     RenderableMP4,
     RenderableSGEO,
     RenderableVNC,
+    RenderableVNCAngular,
     RenderableWebGL,
 )
 import requests
@@ -826,6 +827,9 @@ class Session:
             render = RenderableVNC(self, **kwargs)
         elif what == "remote_scene":
             render = RenderableEVSN(self, **kwargs)
+        # Undocumented. Available only internally
+        elif what == "webensight":
+            render = RenderableVNCAngular(self, **kwargs)
 
         if render is None:
             raise RuntimeError("Unable to generate requested visualization")
