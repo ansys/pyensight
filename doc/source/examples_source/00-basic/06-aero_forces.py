3783bcb9d27ab028ccf069e8869483ee12687096
Mario Ostieri <mario.ostieri@ansys.com>
2024-05-23 10:07:20 +0100

    fix style and address some of the comments

--- doc/source/examples_source/00-basic/06-aero_forces.py
+++ doc/source/examples_source/00-basic/06-aero_forces.py
@@ -4,7 +4,7 @@
 Calculate the Net Aerodynamic Forces
 =======================================================
 
-Utilze EnSight Net Force Tool in order 
+Utilze EnSight Net Force Tool in order
 to Calculate the Net Aerodynamic Forces
 
 Intended to work with EnSight version 24.2 or later
@@ -17,38 +17,33 @@ Intended to work with EnSight version 24.2 or later
 # Launch and connect to an instance of EnSight.
 # This example uses a local EnSight installation.
 import math
-import os
-from ansys.pyensight.core import LocalLauncher
-#
-# batch is default (no visible ensight session)
-session = LocalLauncher().start()
-# use below for interactive debugging with a visible ensight session 
-#session = LocalLauncher(batch=False).start()
 
-# Setup shortcuts for long winded calls.
-eocore = session.ensight.objs.core
-eoutil = session.ensight.utils
+from ansys.pyensight.core import LocalLauncher
+
+session = LocalLauncher().start()
 
 ###############################################################################
 # Load a dataset
+# --------------
+# Load RC Plane case included in the https://github.com/ansys/example-data
+# repository
 #
+
 path = session.download_pyansys_example("RC_Plane", "pyensight", folder=True)
 session.load_data(os.path.join(path, "extra300_RC_Plane_cpp.case"))
 session.show("image", width=800, height=600)
-#
-# calc forces
-#
+
 body_parts = [
-        "canopy",
-        "fuselage",
-        "horizontal_stabilizer",
-        "nose",
-        "vertical_stabilizer",
-        "wing_lower_surface",
-        "wing_te",
-        "wing_tip",
-        "wing_upper_surface",
-    ]
+    "canopy",
+    "fuselage",
+    "horizontal_stabilizer",
+    "nose",
+    "vertical_stabilizer",
+    "wing_lower_surface",
+    "wing_te",
+    "wing_tip",
+    "wing_upper_surface",
+]
 
 vref = session.ensight.utils.variables.get_const_val("Vinf")
 dref = session.ensight.utils.variables.get_const_val("Rho")
@@ -58,16 +53,19 @@ vxref = vref * math.cos(aoa * math.pi / 180)
 vyref = vref * math.sin(aoa * math.pi / 180)
 vzref = 0
 area_ref = None
-if session.ensight.utils.variables._calc_var(["wing_upper_surface"], "area_ref = Area(plist)"):
-    area_ref = session.ensight.utils.variables.get_const_val("area_ref")
+session.ensight.utils.variables.calculator.area(["wing_upper_surface"], output_varname="area_ref")
+area_ref = session.ensight.utils.variables.get_const_val("area_ref")
 
 if not area_ref:
     raise RuntimeError("The reference area could not be calculated")
-#
+
+
+###############################################################################
 #  Compute forces on 2D parts
 # .. image:: /_static/06_aero_forces_1.png
 #
-session.ensight.utils.variables.compute_forces(
+
+forces = session.ensight.utils.variables.compute_forces(
     pobj_list=body_parts.copy(),
     press_var_obj="staticPressure",
     shear_var_obj="wallShearStress",
@@ -80,41 +78,54 @@ session.ensight.utils.variables.compute_forces(
     velocity_z_ref=vzref,
     up_vector=session.ensight.utils.variables.UP_VECTOR_PLUS_Y,
 )
-#assert os.path.exists("test.csv")
-session.ensight.part.select_begin(1,5,7,8,9,10)
+
+# Hide 3D parts and mirror geometry
+session.ensight.part.select_begin(1, 5, 7, 8, 9, 10)
 session.ensight.part.modify_begin()
 session.ensight.part.visible("OFF")
 session.ensight.part.modify_end()
-session.ensight.part.select_begin(2,3,4,6,11,12,13,14,15)
+session.ensight.part.select_begin(2, 3, 4, 6, 11, 12, 13, 14, 15)
 session.ensight.view_transf.fit(0)
-session.ensight.part.select_begin(2,3,4,6,11,12,13,14,15)
+session.ensight.part.select_begin(2, 3, 4, 6, 11, 12, 13, 14, 15)
 session.ensight.part.modify_begin()
 session.ensight.part.symmetry_type("mirror")
 session.ensight.part.modify_end()
-session.ensight.part.select_begin(2,3,4,6,11,12,13,14,15)
+session.ensight.part.select_begin(2, 3, 4, 6, 11, 12, 13, 14, 15)
 session.ensight.part.modify_begin()
 session.ensight.part.symmetry_mirror_x("ON")
 session.ensight.part.modify_end()
 session.ensight.view_transf.fit(0)
-session.ensight.view_transf.rotate(16.3245811,44.9999962,0)
+session.ensight.view_transf.rotate(16.3245811, 44.9999962, 0)
 session.ensight.view.highlight_parts("OFF")
-#
-#
-# 
+
 session.show("image", width=800, height=600)
-gv = session.ensight.utils.variables.get_const_val
+
+###############################################################################
+#  Print out the computed forces
+# ----------------------------------------------------------
+# The compute_forces function returns a dictionary that can be easily
+# walked to get all the results
 #
-#  Print out results
-#
-per_part_pressure_var_list = ['ENS_Force_Net_press_X','ENS_Force_Net_press_Y','ENS_Force_Net_press_Z']
-per_part_shear_var_list = ['ENS_Force_Net_Tan_ShearForce_X','ENS_Force_Net_Tan_ShearForce_Y','ENS_Force_Net_Tan_ShearForce_Z']
-total_net_pressure_var_list= ['ENS_Force_Total_Net_press_X','ENS_Force_Total_Net_press_Y','ENS_Force_Total_Net_press_Z']
-total_net_shear_var_list = ['ENS_Force_Total_Net_Tan_ShearForce_X','ENS_Force_Total_Net_Tan_ShearForce_Y','ENS_Force_Total_Net_Tan_ShearForce_Z']
-#
-print("Per part force X")
-print(body_parts[0], gv(per_part_pressure_var_list[0],body_parts[0]))
-print(body_parts[0], gv(per_part_shear_var_list[0],body_parts[0]))
-print("Total Force")
-print("Total (Net) Pressure Force X = ",gv(total_net_pressure_var_list[0]))
-print("Total (Net) Shear Force X = ",gv( total_net_shear_var_list[0]))
+
+for idx, axis in enumerate(["X", "Y", "Z"]):
+    print(f"Per part force {axis}")
+    total_pressure_force = 0
+    total_shear_force = 0
+    for part in body_parts:
+        part_press_force = forces["pressure_forces"][part][idx]
+        part_shear_force = forces["shear_forces"][part][idx]
+        print(f"Pressure {axis} force on {part} surface: {part_press_force}")
+        print(f"Shear {axis} force on {part} surface: {part_shear_force}")
+        total_pressure_force += part_press_force
+        total_shear_force += part_shear_force
+    print(f"Total Force along the {axis} direction")
+    print(f"Total (Net) Pressure Force {axis} = {total_pressure_force}")
+    print(f"Total (Net) Shear Force {axis} = {total_shear_force}")
+
+
+###############################################################################
+# Thumbnail
+# sphinx_gallery_thumbnail_path = '_static/06_aero_forces_1.png'
+
+
 session.close()
--- doc/source/examples_source/00-basic/07-volume_render.py
+++ doc/source/examples_source/00-basic/07-volume_render.py
@@ -18,20 +18,8 @@ Intended to work with EnSight version 24.2 or later
 # This example uses a local EnSight installation.
 from ansys.pyensight.core import LocalLauncher
 
-# batch is default (no visible ensight session)
-session = LocalLauncher().start()
+session = LocalLauncher(batch=False).start()
 
-# use below for interactive debugging with a visible ensight session 
-#session = LocalLauncher(batch=False).start()
-
-# You can also specify the version of EnSight to use
-#ansys_loc = r"""C:\Program Files\ANSYS Inc\v242"""
-#session=LocalLauncher(ansys_installation = ansys_loc, batch=False).start()
-
-# Setup shortcuts for long winded calls.
-sesse = session.ensight
-eocore = sesse.objs.core
-eoutil = sesse.utils
 
 ###############################################################################
 # Load a dataset
@@ -56,52 +44,42 @@ session.show("image", width=800, height=600)
 #  particular IJK range for the surface of the shuttle
 # ------------------------------------------------------------------
 
-sesse.data_partbuild.begin()
-sesse.case.select("Case 1")
-sesse.data_partbuild.data_type("structured")
-sesse.data_partbuild.group("OFF")
-sesse.data_partbuild.select_begin(1)
-sesse.data_partbuild.domain("all")
-sesse.data_partbuild.noderange_i(1, 53)
-sesse.data_partbuild.noderange_j(1, 63)
-sesse.data_partbuild.noderange_k(1, 1)
-sesse.data_partbuild.nodestep(1, 1, 1)
-sesse.data_partbuild.nodedelta(0, 0, 0)
-sesse.data_partbuild.description("Shuttle")
-sesse.data_partbuild.create()
-sesse.part.select_byname_begin("(CASE:Case 1)Shuttle")
-sesse.case.select("Case 1")
-sesse.data_partbuild.end()
+# LPARTS are loadable parts in EnSight. The idea here is to take the
+# only structured part available and build after it a 2D surface, that
+# is the body surface. This is accomplished just selecting the max
+# values of the I,J,K directions and then loading the LPART
+lpart = session.ensight.objs.core.CURRENTCASE[0].LPARTS[0]
+lpart.NODEMAX = [53, 63, 1]
+lpart.load()
+
 session.show("image", width=800, height=600)
 
 ##############################################################################
 # Volume Render Flow
 # ------------------------------------------------------------------
-# 
+#
 # .. image:: /_static/07_volume_render_1.png
 #
 #
 # Make the shuttle surface an uninteresting, uniform color grey
 #
-sesse.part.select_begin(2)
-sesse.part.colorby_palette("none")
-sesse.part.colorby_rgb(0.6,0.6,0.6)
-sesse.view.highlight_parts("OFF")
+
+# PARTS[1] is the body surface, just created
+session.ensight.objs.core.PARTS[1].COLORBYRGB = [0.6, 0.6, 0.6]
+session.ensight.view.highlight_parts("OFF")
+
 #
 # Color the 3D flow field by the variable that we
 #  wish to visualize using Volume Rendering, Mach
 #
-sesse.part.select_begin(1)
-sesse.part.modify_begin()
-sesse.part.colorby_palette("Mach")
-sesse.legend.select_palette_begin("Mach")
-sesse.part.modify_end()
-sesse.legend.select_palette_begin("Mach")
-sesse.function.palette("Mach")
-sesse.function.palette("Mach")
-sesse.function.modify_begin()
-sesse.function.restore_predefinedpal("use_new_levels","Magma")
-sesse.function.modify_end()
+
+# PARTS[0] is the 3D flow part
+session.ensight.objs.core.PARTS[0].COLORBYPALETTE = "Mach"
+session.ensight.function.palette("Mach")
+session.ensight.function.modify_begin()
+session.ensight.function.restore_predefinedpal("use_new_levels", "Magma")
+session.ensight.function.modify_end()
+
 #
 #  Volume Rendering, in EnSight, is accomplished by subdividing
 #   the flow field into a finite number of hex elements and then
@@ -109,85 +87,92 @@ sesse.function.modify_end()
 #   the normalized variable value and the opacity ('alpha') for
 #   ranges of values set using function points and the magnitude
 #
-sesse.part.select_begin(1)
-sesse.clip.begin()
-sesse.part.colorby_palette("Mach")
-sesse.clip.domain("volume")
-sesse.clip.tool("xyz_box")
-sesse.clip.sample_step(128,128,128)
-sesse.clip.origin(-0.497170001,1.27204275,-0.879772604)
+
+session.ensight.part.select_begin(1)
+session.ensight.clip.begin()
+session.ensight.part.colorby_palette("Mach")
+session.ensight.clip.domain("volume")
+session.ensight.clip.tool("xyz_box")
+session.ensight.clip.sample_step(128, 128, 128)
+session.ensight.clip.origin(-0.497170001, 1.27204275, -0.879772604)
 #
-sesse.clip.axis("x",0,0,1)
-sesse.clip.axis("y",0,-1,0)
-sesse.clip.axis("z",1,0,0)
+session.ensight.clip.axis("x", 0, 0, 1)
+session.ensight.clip.axis("y", 0, -1, 0)
+session.ensight.clip.axis("z", 1, 0, 0)
+
+session.ensight.clip.length(2.24244738, 1.39883041, 3.60410643)
+session.ensight.clip.end()
+session.ensight.clip.create()
+session.ensight.legend.width(0.0244169608)
+session.ensight.legend.height(0.662014842)
+session.ensight.function.palette("Mach")
+
+
+session.ensight.function.point(0, "alpha", 0.000000, 0.000000)
+session.ensight.function.point(1, "alpha", 0.061172, 0.000000)
+session.ensight.function.point(2, "alpha", 0.133467, 0.000000)
+session.ensight.function.point(3, "alpha", 0.194639, 0.000000)
+session.ensight.function.point(4, "alpha", 0.236347, 0.899835)
+session.ensight.function.point(5, "alpha", 0.316983, 0.899835)
+session.ensight.function.point(6, "alpha", 0.319764, 0.000000)
+session.ensight.function.point(7, "alpha", 0.361472, 0.000000)
+session.ensight.function.point(8, "alpha", 0.364253, 0.908182)
+session.ensight.function.point(9, "alpha", 0.433767, 0.908182)
+session.ensight.function.point(10, "alpha", 0.444889, 0.000000)
+session.ensight.function.point(11, "alpha", 0.486597, 0.000000)
+
+
+session.ensight.function.add_knot(12, "alpha")
+
+
+session.ensight.function.point(12, "alpha", 0.500500, 0.933223)
+session.ensight.function.point(13, "alpha", 0.611722, 0.883140)
+session.ensight.function.point(14, "alpha", 0.639528, 0.000000)
+session.ensight.function.point(15, "alpha", 0.959292, 0.000000)
+session.ensight.function.point(16, "alpha", 1.000000, 0.899835)
+
 #
-sesse.clip.length(2.24244738,1.39883041,3.60410643)
-sesse.clip.end()
-sesse.clip.create()
-sesse.legend.width(0.0244169608)
-sesse.legend.height(0.662014842)
-sesse.function.palette("Mach")
-#
-#
-#
-sesse.function.point(0,"alpha",0.000000,0.000000)
-sesse.function.point(1,"alpha",0.061172,0.000000)
-sesse.function.point(2,"alpha",0.133467,0.000000)
-sesse.function.point(3,"alpha",0.194639,0.000000)
-sesse.function.point(4,"alpha",0.236347,0.899835)
-sesse.function.point(5,"alpha",0.316983,0.899835)
-sesse.function.point(6,"alpha",0.319764,0.000000)
-sesse.function.point(7,"alpha",0.361472,0.000000)
-sesse.function.point(8,"alpha",0.364253,0.908182)
-sesse.function.point(9,"alpha",0.433767,0.908182)
-sesse.function.point(10,"alpha",0.444889,0.000000)
-sesse.function.point(11,"alpha",0.486597,0.000000)
-#
-#
-sesse.function.add_knot(12,"alpha")
-#
-#
-sesse.function.point(12,"alpha",0.500500,0.933223)
-sesse.function.point(13,"alpha",0.611722,0.883140)
-sesse.function.point(14,"alpha",0.639528,0.000000)
-sesse.function.point(15,"alpha",0.959292,0.000000)
-sesse.function.point(16,"alpha",1.000000,0.899835)
-#
-#  by adjusting the min and max of the variable values
+#  By adjusting the min and max of the variable values
 #  shown, this can isolate the view to only the values
-#  of interest within the alpha filterig.
+#  of interest within the alpha filtering.
 #
-sesse.function.range(0.02,0.504598)
-sesse.function.range(0.02,0.353)
+
+session.ensight.function.range(0.02, 0.504598)
+session.ensight.function.range(0.02, 0.353)
+
 #
 # This is a symmetric analysis about the Y axis
 #
-sesse.viewport.select_begin(0)
-sesse.viewport.background_type("constant")
-sesse.viewport.constant_rgb(0,0,0)
-sesse.part.select_begin(1)
-sesse.part.visible("OFF")
-sesse.part.select_begin(2,3)
-sesse.part.modify_begin()
-sesse.part.symmetry_type("mirror")
-sesse.part.modify_end()
-sesse.part.select_begin(2,3)
-sesse.part.modify_begin()
-sesse.part.symmetry_mirror_y("ON")
-sesse.part.modify_end()
+
+session.ensight.viewport.select_begin(0)
+session.ensight.viewport.background_type("constant")
+session.ensight.viewport.constant_rgb(0, 0, 0)
+session.ensight.part.select_begin(1)
+session.ensight.part.visible("OFF")
+session.ensight.part.select_begin(2, 3)
+session.ensight.part.modify_begin()
+session.ensight.part.symmetry_type("mirror")
+session.ensight.part.modify_end()
+session.ensight.part.select_begin(2, 3)
+session.ensight.part.modify_begin()
+session.ensight.part.symmetry_mirror_y("ON")
+session.ensight.part.modify_end()
+
 #
 #  Adjust the view
 #
-sesse.view_transf.rotate(-0.214030892,0.477030873,0)
-sesse.view_transf.rotate(-82.8465729,5.88338804,0)
-sesse.view_transf.rotate(0.665434599,113.692574,0)
-sesse.view_transf.zoom(0.317107886)
-sesse.view_transf.translate(-0.815654397,0.113996685,0)
-sesse.view_transf.rotate(16.6358604,3.3392241,0)
-sesse.view_transf.rotate(-6.32162857,-5.72438145,0)
-sesse.view_transf.rotate(9.64879894,6.2014122,0)
+
+session.ensight.view_transf.rotate(-0.214030892, 0.477030873, 0)
+session.ensight.view_transf.rotate(-82.8465729, 5.88338804, 0)
+session.ensight.view_transf.rotate(0.665434599, 113.692574, 0)
+session.ensight.view_transf.zoom(0.317107886)
+session.ensight.view_transf.translate(-0.815654397, 0.113996685, 0)
+session.ensight.view_transf.rotate(16.6358604, 3.3392241, 0)
+session.ensight.view_transf.rotate(-6.32162857, -5.72438145, 0)
+session.ensight.view_transf.rotate(9.64879894, 6.2014122, 0)
 
 session.show("image", width=800, height=600)
+
 ###############################################################################
 # Thumbnail
 # sphinx_gallery_thumbnail_path = '_static/07_volume_rendering_3.png'
