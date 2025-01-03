4d6e3a858899ad143a2a034d961ab6e87efe61d8
Mario Ostieri <mario.ostieri@ansys.com>
2025-01-03 15:19:33 +0000

    try to set up the user to be used to run the container

--- src/ansys/pyensight/core/dockerlauncher.py
+++ src/ansys/pyensight/core/dockerlauncher.py
@@ -325,6 +325,7 @@ class DockerLauncher(Launcher):
                         detach=True,
                         auto_remove=True,
                         remove=True,
+                        user="ensight:ensight"
                     )
             else:
                 logging.debug(f"Running container {self._image_name} with cmd {enshell_cmd}\n")
@@ -340,6 +341,7 @@ class DockerLauncher(Launcher):
                         detach=True,
                         auto_remove=True,
                         remove=True,
+                        user="ensight:ensight"
                     )
                 logging.debug(f"_container = {str(self._container)}\n")
         else:
@@ -357,6 +359,7 @@ class DockerLauncher(Launcher):
                         detach=True,
                         auto_remove=True,
                         remove=True,
+                        user="ensight:ensight"
                     )
             else:  # pragma: no cover
                 logging.debug(
@@ -373,6 +376,7 @@ class DockerLauncher(Launcher):
                         detach=True,
                         auto_remove=True,
                         remove=True,
+                        user="ensight:ensight"
                     )
                 # logging.debug(f"_container = {str(self._container)}\n")
         logging.debug("Container started.\n")
