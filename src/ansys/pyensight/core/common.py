d8085fa415b637b8bfb75324081c260197ace05f
Mario Ostieri <mario.ostieri@ansys.com>
2025-11-25 12:05:28 +0000

    fix dockerlauncher missing security token

--- src/ansys/pyensight/core/common.py
+++ src/ansys/pyensight/core/common.py
@@ -169,6 +169,7 @@ def launch_enshell_interface(
     enshell_grpc_channel: Any,
     grpc_port: int,
     timeout: float,
+    secret_key: str,
     grpc_use_tcp_sockets: bool = False,
     grpc_allow_network_connections: bool = False,
     grpc_disable_tls: bool = False,
@@ -213,6 +214,7 @@ def launch_enshell_interface(
             grpc_disable_tls=grpc_disable_tls,
             grpc_uds_pathname=grpc_uds_pathname,
         )  # pragma: no cover
+        enshell.security_token = secret_key
         enshell.connect_existing_channel(enshell_grpc_channel)  # pragma: no cover
     else:
         enshell = enshell_grpc.EnShellGRPC(
@@ -222,6 +224,7 @@ def launch_enshell_interface(
             grpc_disable_tls=grpc_disable_tls,
             grpc_uds_pathname=grpc_uds_pathname,
         )
+        enshell.security_token = secret_key
         time_start = time.time()
         while time.time() - time_start < timeout:  # pragma: no cover
             if enshell.is_connected():
--- src/ansys/pyensight/core/dockerlauncher.py
+++ src/ansys/pyensight/core/dockerlauncher.py
@@ -460,7 +460,14 @@ class DockerLauncher(Launcher):
         #
         # Start up the EnShell gRPC interface
         self._enshell = launch_enshell_interface(
-            self._enshell_grpc_channel, self._service_host_port["grpc"][1], self._timeout
+            self._enshell_grpc_channel,
+            self._service_host_port["grpc"][1],
+            self._timeout,
+            self._secret_key,
+            grpc_allow_network_connections=self._grpc_allow_network_connections,
+            grpc_disable_tls=self._grpc_disable_tls,
+            grpc_uds_pathname=self._grpc_uds_pathname,
+            grpc_use_tcp_sockets=self._grpc_use_tcp_sockets,
         )
         log_dir = "/data"
         if self._enshell_grpc_channel:  # pragma: no cover
