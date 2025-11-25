ec41ab23eda2a5bf2a1cb3faa90aa17a1d7fcf02
Mario Ostieri <mario.ostieri@ansys.com>
2025-11-25 17:27:19 +0000

    fix omniverse cli and sos test

--- src/ansys/pyensight/core/utils/omniverse_cli.py
+++ src/ansys/pyensight/core/utils/omniverse_cli.py
@@ -242,6 +242,9 @@ class OmniverseGeometryServer(object):
             time_scale=self.time_scale,
             handler=update_handler,
             uds_path=uds_path,
+            grpc_disable_tls=self._grpc_disable_tls,
+            grpc_allow_network_connections=self._grpc_allow_network_connections,
+            grpc_use_tcp_sockets=self._grpc_use_tcp_sockets,
         )
 
         # Start the DSG link
@@ -564,6 +567,9 @@ if __name__ == "__main__":
         vrmode=not args.include_camera,
         temporal=args.temporal,
         line_width=line_width,
+        grpc_disable_tls=args.grpc_disable_tls,
+        grpc_allow_network_connections=args.grpc_allow_network_connections,
+        grpc_use_tcp_sockets=args.grpc_use_tcp_sockets,
     )
 
     # run the server
