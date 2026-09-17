025bf9d2b0804fd624602813ae794adc0a38b963
Mario Ostieri <mostieri@synopsys.com>
2026-09-17 16:56:16 +0100

    add nexusdemo to links not to check

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -85,6 +85,7 @@ linkcheck_ignore = [
     r"http://localhost:[0-9]+/",
     r"https://cubit.sandia.gov/public/verdict.html",
     r"https://github.com/ansys/pyensight/issues",
+    r"https://nexusdemo.ensight.com",
 ]
 
 
